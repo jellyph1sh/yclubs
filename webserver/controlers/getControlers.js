470e00aa0a809045450ae563fdd3e5629fa58e72
matiasbellaud <bellaud.matias@gmail.com>
2024-01-10 13:56:37 +0100

    :spark: | create new controler

--- webserver/controlers/getControlers.js
+++ webserver/controlers/getControlers.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 const DB_PATH = "./clubs.db";
 const Verif = require("../verificationFunc/verifInput.js");
 const moment = require("moment");
+const hashFunc = require("../verificationFunc/password.js")
 
 exports.getClubs = async (req, res) => {
   let clubs = await Database.Read(
@@ -10,23 +11,6 @@ exports.getClubs = async (req, res) => {
     "SELECT idClub,idClubParent,name,description,capital FROM clubs;"
   );
   res.json(clubs);
-};
-
-exports.getUsers = async (req, res) => {
-  console.log("test")
-  let users = await Database.Read(
-    DB_PATH,
-    "SELECT idUser,lastname,firstname,email,password,isAdmin FROM users;"
-  );
-  res.json(users);
-};
-
-exports.getEvents = async (req, res) => {
-  const events = await Database.Read(
-    DB_PATH,
-    "SELECT idEvent,idClub,name,description FROM events;"
-  );
-  res.json(events);
 };
 
 exports.getOneClubsByName = async (clubName) => {
@@ -38,3 +22,69 @@ exports.getOneClubsByName = async (clubName) => {
   let club = clubs[0];
   return club;
 };
+
+
+exports.getLastClubs = async (req, res) => {
+  let clubs = await Database.Read(
+    DB_PATH,
+    "SELECT * FROM clubs ORDER BY idClub DESC LIMIT 1 ;"
+  );
+  res.json(clubs);
+};
+
+exports.getNbrClubs = async (req, res) => {
+  let nbrClubs = await Database.Read(
+    DB_PATH,
+    "SELECT COUNT(idClub) FROM clubs;"
+  );
+  res.json(nbrClubs);
+};
+
+exports.getAllUsers = async (req, res) => {
+  let users = await Database.Read(
+    DB_PATH,
+    "SELECT idUser,lastname,firstname,email,password,isAdmin FROM users;"
+  );
+  res.json(users);
+};
+
+exports.loginUsers = async (req, res) => {
+  const loginUser = req.body
+  let users = await Database.Read(
+    DB_PATH,
+    "SELECT idUser,lastname,firstname,email,password,isAdmin FROM users WHERE email=?;",
+    loginUser.email
+  );
+  if (users[0].password == hashFunc.hashPassword("sha256", "base64", loginUser.password)){
+    res.json(users);
+  }else{
+    res.json({ status: false, error: "Password or email is false" });
+  }
+};
+
+exports.getNbrMembers = async (req, res) => {
+  let nbrMember = await Database.Read(
+    DB_PATH,
+    "SELECT COUNT(DISTINCT idUser) FROM membersClubs;"
+  );
+  res.json(nbrMember);
+};
+
+
+exports.getEvents = async (req, res) => {
+  const events = await Database.Read(
+    DB_PATH,
+    "SELECT idEvent,idClub,name,description FROM events;"
+  );
+  res.json(events);
+};
+
+exports.get3LastEvents = async (req, res) => {
+  const events = await Database.Read(
+    DB_PATH,
+    "SELECT * FROM events ORDER BY idEvent DESC LIMIT 3 ;"
+  );
+  res.json(events);
+};
+
+
--- webserver/routes.js
+++ webserver/routes.js
@@ -6,39 +6,43 @@ const stuffCtrlGet = require("./controlers/getControlers.js");
 const stuffCtrlUpdate = require("./controlers/updateControlers.js");
 const stuffCtrlDelete = require("./controlers/deleteControlers.js");
 
-//clubs
+// CLUBS
 router.get("/clubs/getall", stuffCtrlGet.getClubs);
+router.get("/clubs/getLast", stuffCtrlGet.getLastClubs);
 // router.get("/clubs/get_one", stuffCtrlGet.getOneClubs);
+router.get("/clubs/getNbrClub", stuffCtrlGet.getNbrClubs);
 router.post("/clubs/add", stuffCtrlAdd.addClub);
 router.post("/clubs/update", stuffCtrlUpdate.updateClub);
 // router.delete('/clubs/delete',stuffCtrlDelete.Club)
 
-//users
-router.get("/users/getall", stuffCtrlGet.getUsers);
-// router.get("/users/get_one", stuffCtrlGet.getOneUsers);
+// USERS
+router.get("/users/getall", stuffCtrlGet.getAllUsers);
+router.get("/users/login", stuffCtrlGet.loginUsers);
 router.post("/users/add", stuffCtrlAdd.addUser);
 
-//clubMember
+// CLUB MEMBERS
 router.post("/clubsMembers/add", stuffCtrlAdd.addClubMember);
+router.get("/clubsMembers/getNbrMembers", stuffCtrlGet.getNbrMembers);
 // router.post("/clubsMembers/update", stuffCtrlUpdate.updateMember);
 router.post("/clubsMembers/updateRoles", stuffCtrlUpdate.updateRoleMember);
 // router.delete('/clubsMembers/delete',stuffCtrlDelete.ClubMember)
 
-//roles
+// ROLES
 router.post("/roles/add", stuffCtrlAdd.addRole);
 // router.delete("/roles/delete", stuffCtrlDelete.Role);
 
-//tag
+// TAGS
 router.post("/tags/addToClub", stuffCtrlAdd.addTagToClub);
 router.delete('/tags/deleteToClubs',stuffCtrlDelete.deleteTagClub)
 
 
-//event
+// EVENTS
 router.get("/events/getall", stuffCtrlGet.getEvents);
+router.get("/events/get3Last", stuffCtrlGet.get3LastEvents);
 router.post("/events/add", stuffCtrlAdd.addEvent);
 // router.get("/events/getClub", stuffCtrlGet.getClubEvent);
-// router.get("/events/getOne", stuffCtrl.getOneEvent);
 
+// CAPITAL
 router.post("/capital/update", stuffCtrlUpdate.updateCapitalClub);
 
 module.exports = router;
