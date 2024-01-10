4d1fdb63baa7710f679ff6ed3db6dfa353e6f698
fevan <evan.ferron53@gmail.com>
2024-01-10 09:28:23 +0100

    :art: | reorganize files

---
+++ webserver/controlers/deleteControlers.js
@@ -0,0 +1,21 @@
+const Database = require("../Database.js");
+const crypto = require("crypto");
+const DB_PATH = "../clubs.db";
+const Verif = require("../verificationFunc/verifInput.js");
+const moment = require("moment");
+
+exports.deleteTagClub = async (req, res) => {
+  const tag = req.body;
+  err = await Database.Write(
+    DB_PATH,
+    "DELETE FROM tags WHERE idClub=? AND idTag=?;",
+    tag.idClub,
+    tag.idTag
+  );
+  if (err != null) {
+    console.error(err);
+    res.json({ status: false });
+    return;
+  }
+  res.json({ status: true });
+};
---
+++ webserver/controlers/getControlers.js
@@ -0,0 +1,39 @@
+const Database = require("../Database.js");
+const crypto = require("crypto");
+const DB_PATH = "../clubs.db";
+const Verif = require("../verificationFunc/verifInput.js");
+const moment = require("moment");
+
+exports.getClubs = async (req, res) => {
+  let clubs = await Database.Read(
+    DB_PATH,
+    "SELECT idClub,idClubParent,name,description,capital FROM clubs;"
+  );
+  res.json(clubs);
+};
+
+exports.getUsers = async (req, res) => {
+  let users = await Database.Read(
+    DB_PATH,
+    "SELECT lastname,firstname,email,password,isAdmin FROM users;"
+  );
+  res.json(users);
+};
+
+exports.getEvents = async (req, res) => {
+  const events = await Database.Read(
+    DB_PATH,
+    "SELECT idEvent,idClub,name,description FROM events;"
+  );
+  res.json(events);
+};
+
+exports.getOneClubsByName = async (clubName) => {
+  let clubs = await Database.Read(
+    DB_PATH,
+    "SELECT * FROM clubs WHERE name=?;",
+    clubName
+  );
+  let club = clubs[0];
+  return club;
+};
