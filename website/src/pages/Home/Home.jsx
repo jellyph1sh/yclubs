465cc3a025694906f04a0623937373f74dea5df9
jellyph1sh <sacha.sorgiati@gmail.com>
2024-01-10 11:09:48 +0100

    :sparkles: :lipstick: | Add events box and event notification. Responsive home page.

--- website/src/pages/Home/Home.jsx
+++ website/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import NaviguationBar from "../../components/NaviguationBar/NaviguationBar";
 import StatisticsBox from "../../components/StatisticsBox/StatisticsBox";
 import LastClubBox from "../../components/LastClubBox/LastClubBox";
+import EventsBox from "../../components/EventsBox/EventsBox";
 import "./Home.css";
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <NaviguationBar/>
       <StatisticsBox/>
       <LastClubBox/>
+      <EventsBox/>
     </>
   )
 };
