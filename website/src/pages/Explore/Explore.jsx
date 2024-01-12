ebad26bff4ea4de9e83c1d9798cfba7690d119ff
fevan <evan.ferron53@gmail.com>
2024-01-12 08:17:55 +0100

    :bug: | fix 'missing key error' explore page

--- website/src/pages/Explore/Explore.jsx
+++ website/src/pages/Explore/Explore.jsx
@@ -42,7 +42,7 @@ const Explore = () => {
                 <div className="list-cards">
                     {
                     clubs.map(club => (
-                        <ClubsCard info={club}/>
+                        <ClubsCard info={club} key={club.idClub}/>
                     ))
                     }
                 </div>
