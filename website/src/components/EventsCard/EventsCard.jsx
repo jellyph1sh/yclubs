0e6afa43eaee4bc2805e0ad147787c89214853cd
jellyph1sh <sacha.sorgiati@gmail.com>
2024-01-12 02:02:13 +0100

    :lipstick: :sparkles: | Fix responsive and style, add link with api.

--- website/src/components/EventsCard/EventsCard.jsx
+++ website/src/components/EventsCard/EventsCard.jsx
@@ -1,14 +1,14 @@
 import "./EventsCard.css";
 
-const EventsCard = () => {
+const EventsCard = (props) => {
     return (
         <div className="ec-box">
-            <p className="ec-date">07/05/2024</p>
+            <p className="ec-date">{props.info.date}</p>
             <div className="ec-titles">
-                <h3 className="ec-title">Tournoi de volley</h3>
-                <h4 className="ec-alias">BDS</h4>
+                <h3 className="ec-title">{props.info.name}</h3>
+                <h4 className="ec-alias">{props.info.alias}</h4>
             </div>
-            <p className="ec-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, alias. Nihil minus dicta dolore illo amet iusto similique neque atque quas repudiandae, quod id soluta voluptatum obcaecati laborum architecto quos.</p>
+            <p className="ec-desc">{props.info.description}</p>
             <button className="ec-button" onClick={() => {console.log("Je participe!")}}>Participer</button>
         </div>
     );
