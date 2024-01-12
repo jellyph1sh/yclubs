8df807c580fe6385eb534394b8b47ed0c3a411fc
matiasbellaud <bellaud.matias@gmail.com>
2024-01-12 08:18:20 +0100

    :sparkles: | add club page

--- website/src/config/Ways.jsx
+++ website/src/config/Ways.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import Club from "../pages/Club/Club";
 
 const Ways = () => {
     return (
@@ -9,6 +10,7 @@ const Ways = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login/>}/>
+            <Route path="/club" element={<Club/>}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="*" element={<Home/>}/>
         </Routes>
