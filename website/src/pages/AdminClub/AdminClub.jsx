7934404fc6f68299bd392bfda332d325c1bf4043
jellyph1sh <sacha.sorgiati@gmail.com>
2024-01-12 03:54:01 +0100

    :sparkles: | Add ClubInfo and MembersBox components to AdminClub page.

--- website/src/pages/AdminClub/AdminClub.jsx
+++ website/src/pages/AdminClub/AdminClub.jsx
@@ -2,6 +2,8 @@ import NaviguationBar from "../../components/NaviguationBar/NaviguationBar";
 import { useCookies } from "react-cookie";
 import "./AdminClub.css";
 import { useNavigate } from "react-router-dom";
+import ClubInfo from "../../components/ClubInfo/ClubInfo";
+import MembersBox from "../../components/MembersBox/MembersBox";
 
 const AdminClub = () => {
     /*const [cookies] = useCookies(["user", "token"]);
@@ -18,7 +20,12 @@ const AdminClub = () => {
         <>
             <NaviguationBar/>
             <div className="ac-box">
-                <h2 className="ac-title">Gestion Club</h2>
+                <div className="ac-header">
+                    <h2 className="ac-title">Gestion Club de Tennis</h2>
+                    <h3 className="ac-balance">Solde: <span className="bold">200€</span></h3>
+                    <ClubInfo/>
+                    <MembersBox/>
+                </div>
             </div>
         </>
     );
